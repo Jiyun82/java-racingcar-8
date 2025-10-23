8d1fa4feae9572add289dabc42cd00e1516086db
Jiyun82 <202110918@sangmyung.kr>
2025-10-23 16:44:53 +0900

    feat: 입력을 받아 CarFactory로 자동차 리스트 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        CarFactory carFactory = new CarFactory();
+        List<Car> cars = carFactory.createCars(input);
+
     }
 }
---
+++ src/main/java/racingcar/CarFactory.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarFactory {
+
+    public List<Car> createCars(String input) {
+        String[] names = input.split(",");
+        List<Car> cars = new ArrayList<>();
+        for(String name : names) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+
+}
