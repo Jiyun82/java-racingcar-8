1e92f669fe494be4157506430adfd6ae86923e78
Jiyun82 <202110918@sangmyung.kr>
2025-10-23 23:22:22 +0900

    fix: 입력받은 이름 앞 뒤 공백 제거

--- src/main/java/racingcar/CarFactory.java
+++ src/main/java/racingcar/CarFactory.java
@@ -6,10 +6,11 @@ import java.util.List;
 public class CarFactory {
 
     public List<Car> createCars(String input) {
+
         String[] names = input.split(",");
         List<Car> cars = new ArrayList<>();
         for(String name : names) {
-            cars.add(new Car(name));
+            cars.add(new Car(name.trim()));
         }
         return cars;
     }
