867f355ded1751a00ac816f7e84fb71b3d7d2e14
Jiyun82 <202110918@sangmyung.kr>
2025-10-23 23:12:30 +0900

    fix: 자동차 이름 공백일때 예외 던지도록 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -44,8 +44,8 @@ public class Car {
     }
 
     private void validateName(String name) {
-        if(name.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+        if(name.length() > 5 || name.length() < 1) {
+            throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하만 가능합니다.");
         }
     }
 
