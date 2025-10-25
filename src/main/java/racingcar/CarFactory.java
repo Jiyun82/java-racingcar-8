2d15cbdc9e4f5befbe11e24158f1e05a18cb6bc3
Jiyun82 <202110918@sangmyung.kr>
2025-10-25 16:27:10 +0900

    fix: 중복된 이름 입력 시 예외 던지도록 수정

--- src/main/java/racingcar/CarFactory.java
+++ src/main/java/racingcar/CarFactory.java
@@ -1,13 +1,22 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class CarFactory {
 
     public List<Car> createCars(String input) {
+        List<String> names = Arrays.stream(input.split(","))
+                .map(String::trim)
+                .toList();
 
-        String[] names = input.split(",");
+        HashSet<String> nameSet = new HashSet<>(names);
+        if(nameSet.size() < names.size()) {
+            throw new IllegalArgumentException("중복된 이름은 사용할 수 없습니다.");
+        }
+
         List<Car> cars = new ArrayList<>();
         for (String name : names) {
             cars.add(new Car(name.trim()));
